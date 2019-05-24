37ff2a7ba02a2daab2f4a2c14987fea81380278f
Zephyr <ixuhefeng@gmail.com>
2019-05-24 19:56:27 +0800

    within 2km and parking

--- main/main.js
+++ main/main.js
@@ -1,4 +1,6 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    var distance_price = 6
+    var parkPrice = inputs['parkTime'] * 0.25
+    return Math.round(distance_price + parkPrice);
 };
