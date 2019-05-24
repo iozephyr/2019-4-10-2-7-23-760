31f8d8e177c603b51742d3d3528ebf1de2978848
Zephyr <ixuhefeng@gmail.com>
2019-05-24 20:02:47 +0800

    Refactoring

--- main/main.js
+++ main/main.js
@@ -1,6 +1,18 @@
 module.exports = function main(inputs) {
     // write your code here...
-    var distance_price = 6
-    var parkPrice = inputs['parkTime'] * 0.25
-    return Math.round(distance_price + parkPrice);
+    var distancePrice = StratingPrice()
+    var parkingFee = ParkingFee(inputs['parkTime'])
+    return ShouldPay(distancePrice, parkingFee)
 };
+
+function StratingPrice() {
+    return 6
+}
+
+function ParkingFee(parkTime) {
+    return parkTime * 0.25
+}
+
+function ShouldPay(distancePrice, parkingFee) {
+    return Math.round(distancePrice + parkingFee)
+}
