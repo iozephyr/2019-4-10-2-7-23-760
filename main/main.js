07b2b848949db37966c77f9c692e06c8bd79ddb0
Zephyr <ixuhefeng@gmail.com>
2019-05-24 20:47:24 +0800

    Refactoring

--- main/main.js
+++ main/main.js
@@ -1,18 +1,35 @@
 module.exports = function main(inputs) {
     // write your code here...
-    var distancePrice = StratingPrice()
-    if (inputs['distance'] > 2) {
-        distancePrice += Math.floor(inputs['distance'] - 2) * 0.8
-    }
-    if (inputs['distance'] > 8) {
-        distancePrice += Math.floor(inputs['distance'] - 8) * 0.4
-    }
-    var parkingFee = ParkingFee(inputs['parkTime'])
+    var distance = inputs['distance']
+    var parkTime = inputs['parkTime']
+    var distancePrice = StratingPrice() + PreKMFee(distance) + ExtraFee(distance)
+    var parkingFee = ParkingFee(parkTime)
     return ShouldPay(distancePrice, parkingFee)
 };
 
+var PRE_KM_PRICE = 0.8
+
 function StratingPrice() {
+    // 起步价
     return 6
+}
+
+function PreKMFee(distance) {
+    // 两公里后每公里收费
+    var price = 0
+    if (distance > 2) {
+        price = Math.floor(distance - 2) * PRE_KM_PRICE
+    }
+    return price
+}
+
+function ExtraFee(distance) {
+    // 超出8公里后，每公里加收50%
+    var price = 0
+    if (distance > 8) {
+        price = Math.floor(distance - 8) * PRE_KM_PRICE * 0.5
+    }
+    return price
 }
 
 function ParkingFee(parkTime) {
