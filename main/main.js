704e96cdfd37b4219542de1d34f6953bbc27c98b
Zephyr <ixuhefeng@gmail.com>
2019-05-24 20:22:53 +0800

    between 2km and 8km

--- main/main.js
+++ main/main.js
@@ -1,6 +1,9 @@
 module.exports = function main(inputs) {
     // write your code here...
     var distancePrice = StratingPrice()
+    if (inputs['distance'] > 2) {
+        distancePrice += Math.floor(inputs['distance'] - 2) * 0.8
+    }
     var parkingFee = ParkingFee(inputs['parkTime'])
     return ShouldPay(distancePrice, parkingFee)
 };
@@ -14,5 +17,6 @@ function ParkingFee(parkTime) {
 }
 
 function ShouldPay(distancePrice, parkingFee) {
+    // return distancePrice + parkingFee
     return Math.round(distancePrice + parkingFee)
 }
